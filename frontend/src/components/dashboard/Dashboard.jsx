b3686d44054a3d89303d5aeaccb00c3becee2244
Gaurav Kumar <indiagauravkumar@gmail.com>
2024-01-06 03:09:08 +0530

    Implemented the Dashboard Page.

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import SideNav from "./SideNav"; // Adjust the import path as needed
 export default function Dashboard({ children }) {
 	return (
 		<div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-			<div className="w-full flex-none md:w-64">
+			<div className="w-full flex-none md:w-64 ">
 				<SideNav />
 			</div>
 			<div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
